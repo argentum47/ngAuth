3b9413906d8b065e1b78c91f9b965d6d4ebe180e
Amitava Ghosh <princeofpersiaa3.ag@gmail.com>
2015-05-19 13:44:49 +0530

    Update googleProvider.js

--- googleProvider.js
+++ googleProvider.js
@@ -3,15 +3,9 @@
       .module('ngGoogle', [])
       .provider('$google', function () {
         var config = {
-          permissions: ' https://www.googleapis.com/auth/userinfo.email',
+          permissions: 'email',
           clientId: null
         };
-
-        /* this.setClientSecret = function(apiKey) {
-         config.setApiKey = apiKey;
-         return this;
-         };
-         */
 
         this.setClientId = function(clientId) {
           config.clientId = clientId;
@@ -39,7 +33,6 @@
 
             $google.init = function () {
               $google.resolve($window.gapi);
-              console.log($window.gapi, 1)
             };
 
             $google.authorize = function () {
@@ -49,12 +42,31 @@
                 throw "Authentication failure, client id is missing";
 
               return $google.promise.then(function (gapi) {
-                gapi.auth.authorize({client_id: $google.config("clientId"),
-                  scope: $google.config("permissions"), immediate: true},
-                    function (response) {
-                      if (response.error) deferred.reject(response.error)
+                gapi.auth.authorize({
+                    client_id: $google.config("clientId"),
+                    scope: $google.config("permissions"),
+                    immediate: false
+                  },
+                  function (response) {
+                    if (response.error) deferred.reject(response.error)
+                    else deferred.resolve(response)
+                  });
+
+                return deferred.promise;
+              });
+            };
+
+            $google.me = function() {
+              var deferred = $q.defer();
+
+              return $google.promise.then(function(gapi) {
+                gapi.client.load('oauth2', 'v2', function() {
+                  gapi.client.oauth2.userinfo.get()
+                    .execute(function(response) {
+                      if(response.error) deferred.reject(response.error)
                       else deferred.resolve(response)
                     });
+                  });
 
                 return deferred.promise;
               });
